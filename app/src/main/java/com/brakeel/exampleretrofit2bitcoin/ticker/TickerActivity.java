74987a70633b33af44bd60ef856aca66950d190e
Kelvis Borges <kelvishardwork@gmail.com>
2018-02-05 19:33:13 -0200

    Criação, salvar no banco e lista no cardview

--- app/src/main/java/com/brakeel/exampleretrofit2bitcoin/ticker/TickerActivity.java
+++ app/src/main/java/com/brakeel/exampleretrofit2bitcoin/ticker/TickerActivity.java
@@ -1,12 +1,19 @@
 package com.brakeel.exampleretrofit2bitcoin.ticker;
 
 import android.content.Context;
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.RelativeLayout;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import com.brakeel.exampleretrofit2bitcoin.MainActivity;
 import com.brakeel.exampleretrofit2bitcoin.R;
 import com.brakeel.exampleretrofit2bitcoin.api.ApiClient;
 import com.brakeel.exampleretrofit2bitcoin.api.ApiService;
@@ -15,6 +22,7 @@ import com.brakeel.exampleretrofit2bitcoin.entity.DigitalCurrency;
 import com.brakeel.exampleretrofit2bitcoin.entity.TypeMethod;
 import com.brakeel.exampleretrofit2bitcoin.repository.TickerRepository;
 import com.brakeel.exampleretrofit2bitcoin.util.TickerDeserialize;
+import com.brakeel.exampleretrofit2bitcoin.viewholder.TickerViewHolder;
 import com.google.gson.Gson;
 import com.google.gson.GsonBuilder;
 
@@ -24,13 +32,16 @@ import retrofit2.Response;
 import retrofit2.Retrofit;
 import retrofit2.converter.gson.GsonConverterFactory;
 
-public class TickerActivity extends AppCompatActivity {
+public class TickerActivity extends AppCompatActivity implements View.OnClickListener {
 
     private ViewHolder mViewHolder = new ViewHolder();
     private static final String TAG = "TickerActivity";
     private Ticker tickerModel;
     private Context context;
     private TickerRepository tickerRepository;
+    private Gson gson;
+    private RelativeLayout lytLoading;
+    private TickerViewHolder tickerViewHolder;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -43,16 +54,29 @@ public class TickerActivity extends AppCompatActivity {
         tickerModel = new Ticker();
         tickerRepository = new TickerRepository(this);
 
+        this.mViewHolder.row_ticker_list = findViewById(R.id.row_ticker_list);
         this.mViewHolder.tvTickerLast = (TextView) findViewById(R.id.tvTickerLast);
         this.mViewHolder.tvTickerHigh = (TextView) findViewById(R.id.tvTickerHigh);
         this.mViewHolder.tvTickerLow = (TextView) findViewById(R.id.tvTickerLow);
         this.mViewHolder.tvTickerVol = (TextView) findViewById(R.id.tvTickerVol);
         this.mViewHolder.tvDate = (TextView) findViewById(R.id.tvDate);
 
-
         context = getApplicationContext();
-        Gson gson = new GsonBuilder().registerTypeAdapter(Ticker.class, new TickerDeserialize()).create();
+        gson = new GsonBuilder().registerTypeAdapter(Ticker.class, new TickerDeserialize()).create();
+        lytLoading = findViewById(R.id.lytLoading);
+        setOffLoading();
 
+        requestAPI(gson);
+        mViewHolder.row_ticker_list.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(TickerActivity.this, MainActivity.class));
+            }
+        });
+    }
+
+    private void requestAPI(Gson gson) {
+        setOnLoading();
         Retrofit retrofit = new Retrofit
                 .Builder()
                 .baseUrl(ApiClient.BASE_URL)
@@ -92,14 +116,17 @@ public class TickerActivity extends AppCompatActivity {
                         Log.i(TAG, "Body: " + String.valueOf(response.body()));
                         tickerModel = t;
                         tickerRepository.addTicker(t);
+                        setOffLoading();
                         setData();
+
                     }
                 }
             }
 
             @Override
             public void onFailure(Call<Ticker> call, Throwable t) {
-                Toast.makeText(context, "Por favor, ative a conexão à internet.", Toast.LENGTH_LONG).show();
+                setOffLoading();
+                Toast.makeText(getApplicationContext(), "Erro ao realizar consulta. Verifique sua conexão a internet!", Toast.LENGTH_LONG).show();
             }
         });
     }
@@ -112,12 +139,43 @@ public class TickerActivity extends AppCompatActivity {
         this.mViewHolder.tvDate.setText("Data: " + String.valueOf(this.tickerModel.getDate()));
     }
 
+    @Override
+    public void onClick(View v) {
+
+    }
+
     private static class ViewHolder {
         TextView tvTickerLast;
         TextView tvTickerHigh;
         TextView tvTickerLow;
         TextView tvTickerVol;
         TextView tvDate;
+        View row_ticker_list;
     }
 
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.menu_main, menu);
+
+        return super.onCreateOptionsMenu(menu);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+            case R.id.refresh:
+                requestAPI(gson);
+                break;
+        }
+        return super.onOptionsItemSelected(item);
+    }
+
+    public void setOffLoading() {
+        lytLoading.setVisibility(View.GONE);
+    }
+
+    public void setOnLoading() {
+        lytLoading.setVisibility(View.VISIBLE);
+    }
 }
